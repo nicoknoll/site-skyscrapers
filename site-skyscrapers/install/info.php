aeddf2f360132bb4e1a7ab984352bf07ec40c86f
Nico Knoll <mail@nico-knoll.de>
2014-11-06 20:32:15 +0100

    2.5: Update

---
+++ site-skyscrapers/install/info.php
@@ -0,0 +1,6 @@
+<?php if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "Skyscrapers", 
+	'summary' => "This is the profile for the skyscrapers demo site as seen at: http://processwire.com/skyscrapers/.", 
+	'screenshot' => "skyscrapers-screenshot.jpg"
+	);
